b2a3efbd20768696890d7e8feb851abfe4b9780b
Yatin2505 <yatintembhare1@gmail.com>
2025-07-21 16:55:38 +0530

    Refactor input handling to validate number input and provide feedback for invalid entries

--- + - 0/script.js
+++ + - 0/script.js
@@ -1,6 +1,10 @@
-let a=Number(prompt("Enter the value of a"));
-
-if(a>0) {
+let a=prompt("Enter the value of a");
+let aOriginal=a;
+parseInt(a);
+if(isNaN(a)){
+    document.write(`${aOriginal} is a string value , to check the positive value please enter a valid number !`);
+}
+else if(a>0) {
     document.write(`The number ${a} is positive.`);
 }else if(a<0) {
     document.write(`The number ${a} is negative.`);
