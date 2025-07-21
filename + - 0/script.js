967f53ab893f781e3aa0ad902a4efb454c5cd21f
Yatin Tembhare <yatintembhare1@gmail.com>
2025-07-21 16:48:05 +0530

    one condition added

--- + - 0/script.js
+++ + - 0/script.js
@@ -1,6 +1,9 @@
 let a=Number(prompt("Enter the value of a"));
 
-if(a>0) {
+if(a==isNan()){
+      document.write(`${a} is a string value , to check the positive value please enter a valid number !`);
+}
+else if(a>0) {
     document.write(`The number ${a} is positive.`);
 }else if(a<0) {
     document.write(`The number ${a} is negative.`);
